d5eda40c6b95b04d3d4a0ffeb3d56b8a547a7296
was-siri-us <116163817+was-siri-us@users.noreply.github.com>
2023-02-07 15:11:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,24 +20,24 @@ def returnPred():
         
         if(q[-1]=='1'):
             d['a']=1
-            model = joblib.load('./modelRF.joblib')
-        if(q[-1]=='2'):
-            d['a']=2
-            model = joblib.load('./modelgBoost.joblib')
-        if(q[-1]=='3'):
-            d['a']=3
-            model = joblib.load('./modelLogistic.joblib')
-        if(q[-1]=='4'):
-            d['a']=4
-            model = joblib.load('./modelSVM.joblib')
+#             model = joblib.load('./modelRF.joblib')
+#         if(q[-1]=='2'):
+#             d['a']=2
+#             model = joblib.load('./modelgBoost.joblib')
+#         if(q[-1]=='3'):
+#             d['a']=3
+#             model = joblib.load('./modelLogistic.joblib')
+#         if(q[-1]=='4'):
+#             d['a']=4
+#             model = joblib.load('./modelSVM.joblib')
         
-        out = str(model.predict(answer)[0])
-        if(out=="1" or out=="YES" ):
-            out ="The Water is Potable"
-        else:
-            out="The Water is not Potable"
+#         out = str(model.predict(answer)[0])
+#         if(out=="1" or out=="YES" ):
+#             out ="The Water is Potable"
+#         else:
+#             out="The Water is not Potable"
         
-        d['output'] =out
+#         d['output'] =out
     elif(q[0]=='1'):
        d['output'] = 'pig '
         
